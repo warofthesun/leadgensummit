1a476026c172e5212aa90ed389bc969ed24ddf68
Dan Brown <fireflip@gmail.com>
2019-07-24 15:29:12 -0500

    Add forminator form styles

--- inc/acf/acf-fields.php
+++ inc/acf/acf-fields.php
@@ -324,7 +324,7 @@ acf_add_local_field_group(array(
 		),
 	),
 	'menu_order' => 0,
-	'position' => 'acf_after_title',
+	'position' => 'normal',
 	'style' => 'default',
 	'label_placement' => 'top',
 	'instruction_placement' => 'label',
